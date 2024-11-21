d7e767d57e9a98cd83c26fbff69026db0d875881
aliyan310 <aliyanfarooqiworld@gmail.com>
2024-11-22 01:31:30 +0500

    done

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const input = document.querySelector("#input")
 const button = document.querySelector("#btn")
 const div = document.querySelector(".main")
 
-div.innerHTML =`
+div.innerHTML = `
     <div class="top"><i class="fa-solid fa-up-long"></i></div>
     <h1 class="null">Type City Name</h1>`
 
 
-button.addEventListener("click",()=>{
+button.addEventListener("click", () => {
     // console.log("hellow orld");
     console.log(input.value);
 
-fetch(`https://api.weatherapi.com/v1/current.json?key=46e417ccc1494938bdb160136241811&q=${input.value}&aqi=no`)
-.then((res)=>res.json())
-.then((res)=>{
-    console.log(res);
-    div.innerHTML = `
+    fetch(`https://api.weatherapi.com/v1/current.json?key=46e417ccc1494938bdb160136241811&q=${input.value}&aqi=no`)
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res);
+            div.innerHTML = `
     <div class="navbar">
-    <i class="fa-solid fa-location-dot" ></i><h3 class="grey">${res.location.name} , ${res.location.country}</h3>
+     <h3 class="grey">${res.location.name} , ${res.location.country}</h3>
     </div>
     <div class="section2">
     <h3 class="grey">${res.location.region}</h3>
@@ -36,25 +36,16 @@ fetch(`https://api.weatherapi.com/v1/current.json?key=46e417ccc1494938bdb1601362
     <div class="section2">
         <h2 class="grey">Winds Speed : ${res.current.wind_kph}°C</h2>
     </div>
-    <div class="section2">
-        <h4 class="grey">${res.current.condition.text}</h4>
-    </div>
+    
     <div class="section2">
     <h3 class="grey">Local Time : ${res.location.localtime}</h3>
     </div>
     
-    `    
-})
-.catch((err)=>{
-    console.log(err);
-    Swal.fire({
-  title: "The Internet?",
-  text: "Reload before Enter a valid country Or city",
-  icon: "question"
-});
-    
-    
-})
+    `
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 })
 
 
